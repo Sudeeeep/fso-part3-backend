354ccf76da9bf73f47b3e7803386c0678ede4e4a
Sudeeeep <sudeepn15@gmail.com>
2023-02-21 00:12:28 +0530

    add express.static

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 const PORT = 3001;
 
 app.use(cors());
+app.use(express.static("build"));
 app.use(express.json());
 
 morgan.token("reqBody", (request, response) => JSON.stringify(request.body));
